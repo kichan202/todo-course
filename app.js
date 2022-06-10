bd869c2fec18fcf8e708128a6175463fb01af67c
francisco <franciscodorset@hotmail.com>
2022-06-09 20:26:24 -0400

    removing the todos  functionality is working

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 //change from 'click' to 'change'
 filterOption.addEventListener('change', filterTodo);
+document.addEventListener('DOMContentLoaded', getTodos);
 
 //functions
 function addTodo(event){
@@ -107,4 +108,39 @@ function saveLocalTodos(todo){
     }
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//load the todos from the localstorage when the document loads
+function getTodos(){
+    let todos;
+    if(localStorage.getItem('todos') == null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+        todos.forEach(function(todo){
+            //Create the todo
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add("todo");
+
+            const newTodo = document.createElement('li');
+            newTodo.classList.add("todo-item");
+            newTodo.innerHTML = todo;
+            todoDiv.appendChild(newTodo);
+
+            //Check MArk Button
+            const completedButton = document.createElement('button');
+            completedButton.innerHTML = '<i class="fas fa-check"></i>';
+            completedButton.classList.add("complete-btn");
+            todoDiv.appendChild(completedButton);
+
+            //Check trash button
+            const trashButton = document.createElement('button');
+            trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+            trashButton.classList.add("trash-btn");
+            todoDiv.appendChild(trashButton);
+
+            todoList.appendChild(todoDiv);
+
+        });
+    }
 }
